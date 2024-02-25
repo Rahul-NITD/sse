e63474a37a60d310690f3da1b30d28468773a66e
Rahul-NITD <rahul.goel@pixelotech.com>
2024-02-25 16:39:54 +0530

    refactoring

--- server.go
+++ server.go
@@ -7,35 +7,49 @@ import (
 	"github.com/r3labs/sse/v2"
 )
 
-type ChatHandler struct {
-	http.Handler
-}
+const (
+	HEALTHZ_PATH    = "/healthz"
+	ROOM_PATH       = "/room"
+	DUMP_PATH       = "/dump"
+	ID_PARAM        = "id"
+	DEFAULT_WELCOME = "Hello User"
+)
 
-func NewChatHandler() *ChatHandler {
+func NewChatHandler() http.Handler {
 	r := http.NewServeMux()
 
-	r.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {})
+	sse_server := CreateSSEServer()
 
+	r.HandleFunc(HEALTHZ_PATH, handleHealthZ)
+	r.HandleFunc(ROOM_PATH, sse_server.ServeHTTP)
+	r.HandleFunc(DUMP_PATH, handleDumpDecorator(sse_server))
+
+	return r
+}
+
+func CreateSSEServer() *sse.Server {
 	sse_server := sse.New()
-	sse_server.OnSubscribe = func(streamID string, sub *sse.Subscriber) {
+	sse_server.OnSubscribe = OnSubscribeDecorator(sse_server)
+	sse_server.AutoStream = true
+	return sse_server
+}
+
+func handleHealthZ(w http.ResponseWriter, r *http.Request) {}
+
+func OnSubscribeDecorator(sse_server *sse.Server) func(streamID string, sub *sse.Subscriber) {
+	return func(streamID string, sub *sse.Subscriber) {
 		sse_server.Publish(streamID, &sse.Event{
-			Data: []byte("Hello User"),
+			Data: []byte(DEFAULT_WELCOME),
 		})
 	}
+}
 
-	sse_server.AutoStream = true
-
-	r.HandleFunc("/room", sse_server.ServeHTTP)
-
-	r.HandleFunc("/dump", func(w http.ResponseWriter, r *http.Request) {
+func handleDumpDecorator(sse_server *sse.Server) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 		body, _ := io.ReadAll(r.Body)
-		sse_server.Publish(r.URL.Query().Get("id"), &sse.Event{
+		sse_server.Publish(r.URL.Query().Get(ID_PARAM), &sse.Event{
 			Data: body,
 		})
-	})
-
-	hdlr := &ChatHandler{}
-	hdlr.Handler = r
-	return hdlr
+	}
 }
